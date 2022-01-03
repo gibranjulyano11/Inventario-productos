3e453e0306cde80604ecdf795c2821777bde1b93
gibranjulyano11 <gibranjulyano11@gmail.com>
2022-01-03 11:07:27 -0500

    1 commit

---
+++ Backend/TiendaApi/TiendaApi/Models/Item.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace TiendaApi.Models
+{
+    public class Item
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        [BsonElement("Name")]
+        public string ItemName { get; set; } = null!;
+
+        public decimal Price { get; set; }
+
+        public string Category { get; set; } = null!;
+
+        public string brands { get; set; } = null!;
+
+        public string data { get; set; } = null!;
+
+        public string tags { get; set; } = null!; 
+    }
+}
---
+++ Backend/TiendaApi/TiendaApi/Models/ItemStoreDatabaseSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace TiendaApi.Models
+{
+    public class ItemStoreDatabaseSettings
+    {
+        public string ConnectionStrings { get; set; } = null!;
+
+        public string Database { get; set; } = null!;
+
+        public string ItemsCollectionName { get; set; } = null!;
+    }
+}
